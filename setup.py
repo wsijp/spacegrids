5983b2b708dacb032fa0342785e26a64a4b9d632
willo12 <wim.sijp@gmail.com>
2014-04-15 17:25:23 +1000

    Reorganised directory structure
    To correspond to Python package

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname )).read()
+
+#with open('README.rst') as file:
+#        long_description = file.read()
+
+setup(name='spacegrids',
+      version='1.0.6',
+      author='Willem Sijp',
+      author_email='w.sijp@unsw.edu.au',
+      description='numpy array with grids and associated operations',
+      keywords=('climate data','grid data','data on grids'),
+      packages = find_packages(exclude="tests"),
+#      package_data = {
+#	  "spacegrids": ['README.rst']
+#	  },
+      long_description=read('README'),
+      url='https://github.com/willo12/spacegrids',
+      license = "BSD",
+      install_requires =  ["numpy>=1.6","scipy>=0.10","matplotlib>1.1"]
+#      extras_require = {
+#	  "ncio": ["netCDF4>=1.0.6"],
+#	  "pandas": ["pandas>=0.8.0"],
+#	  "plotting": ["pandas>=0.8.0","matplotlib>=1.2.1"],
+#	  "interp2d": ["basemap>=1.06"],
+#	  }
+      )
